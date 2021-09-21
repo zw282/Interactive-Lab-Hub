357750d8638ddb463aaef8c379865cd30c0dbb46
zw282 <55099696+zw282@users.noreply.github.com>
2021-09-20 21:56:15 -0400

    Create Trip_to_Ithaca.py

---
+++ Lab 2/Trip_to_Ithaca.py
@@ -0,0 +1,119 @@
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
+from datetime import datetime
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Setup the code for our buttons
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+button = digitalio.DigitalInOut(board.D23)
+button.switch_to_input()
+
+# This is the average travelling distance from Roosevelt Island to Ithaca in miles
+main_distance = 234.5
+
+# This is the average driving speed mph
+driving_speed = 55
+
+# This is the average driving time in seconds
+average_time = main_distance / driving_speed * 3600
+
+
+while True:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((0, 0), "Press A", font=font, fill="#F9AD43")
+    draw.text((0, 20), "To start travelling to Ithaca", font=font, fill="#F9AD43")
+    disp.image(image, rotation)
+    
+    
+    end = None
+    
+    if buttonB.value and not buttonA.value:
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        start = time.time()
+        START = 'Start traveling'
+        draw.text((0, 20), START, font=font, fill="#F9AD43")
+        disp.image(image, rotation)
+        time.sleep(0.5)
+        
+        while end == None:
+            if buttonB.value and not buttonA.value:
+                end = time.time()
+        
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        driving_time = end - start
+        distance = (driving_speed * driving_time) / 3600
+        percentage = (driving_time / average_time) * 100
+        percentage = round(percentage,3)
+        distance = round(distance,3)
+        draw.text((0, 0), "You have travelled", font=font, fill="#F9AD43")
+        draw.text((0, 20), str(distance) + "miles", font=font, fill="#F9AD43")
+        draw.text((0, 40), "That is" + str(percentage) + "%", font=font, fill="#F9AD43")
+        draw.text((0, 60), "of the whole trip", font=font, fill="#F9AD43")
+
+        disp.image(image, rotation)
+        time.sleep(5)
+            
