97cba5dca3838888f56f5e765e50adcebe8e0105
rubydongerpan <90153252+rubydongerpan@users.noreply.github.com>
2021-09-26 16:03:52 -0400

    Update new_trip.py

--- Lab 2/new_trip.py
+++ Lab 2/new_trip.py
@@ -120,7 +120,7 @@ while True:
                 draw.text((0, 0), "you are travelling " + str(dirr), font=font, fill="#F9AD43")
                 disp.image(image, rotation)            
                 time.sleep(1)   
-                break
+            break
         
     end1 = None
     
