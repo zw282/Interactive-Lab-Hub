eb04c2eb93e83b47d65b6293023ee1fd485c26d4
rubydongerpan <90153252+rubydongerpan@users.noreply.github.com>
2021-09-26 17:16:05 -0400

    Update new_trip.py

--- Lab 2/new_trip.py
+++ Lab 2/new_trip.py
@@ -98,32 +98,31 @@ while True:
 
     disp.image(image, rotation)
     
-    
-    end = None
-    
-    if buttonB.value and not buttonA.value:
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        
-        dirr = dirlist[0]
-        draw.text((0,0), str(dirr), font=font, fill ="#F9AD43")
-        START = 'Start traveling'
-        disp.image(image, rotation)
-        time.sleep(0.5)
-        
-        if buttonB.value and not buttonA.value:
+    confirm = False
+
+        #direction loop from dir[0]
+        i = 0
+        if buttonB.value and not buttonA.value:            
             draw.rectangle((0, 0, width, height), outline=0, fill=0)
-            dirr = dirlist[1]
+            dirr = dirlist[i]
+            i = i+1
+            if i >3 :
+                i = i % 4 - 1         
             draw.text((0,0), str(dirr), font=font, fill ="#F9AD43")
             disp.image(image, rotation)
-            time.sleep(0.5)
-            
-        while end == None:
+            time.sleep(1)
+
+
             if buttonA.value and not buttonB.value:
-                draw.rectangle((0, 0, width, height), outline=0, fill=0)
-                draw.text((0, 0), "you are travelling " + str(dirr), font=font, fill="#F9AD43")
-                disp.image(image, rotation)            
-                time.sleep(1)
-                
+                confirm = True 
+                continue
+
+        if confirm == True
+            draw.rectangle((0, 0, width, height), outline=0, fill=0)
+            draw.text((0, 0), "you are travelling " + str(dirr), font=font, fill="#F9AD43")
+            disp.image(image, rotation)            
+            time.sleep(1)
+    end = None
     if buttonB.value and not buttonA.value:
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
         start = time.time()
