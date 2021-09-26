b9e7d45df3beb903bcd39d411e10be116bd244fa
zw282 <55099696+zw282@users.noreply.github.com>
2021-09-26 16:22:16 -0400

    Update new_trip.py

--- Lab 2/new_trip.py
+++ Lab 2/new_trip.py
@@ -119,8 +119,8 @@ while True:
                 draw.rectangle((0, 0, width, height), outline=0, fill=0)
                 draw.text((0, 0), "you are travelling " + str(dirr), font=font, fill="#F9AD43")
                 disp.image(image, rotation)            
-                time.sleep(1)   
-        continue
+                time.sleep(0.5)   
+        
         
         end1 = None
 
