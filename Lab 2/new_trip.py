1111cca3d3a8c44379bc3bae11db8bd848ff9463
zw282 <55099696+zw282@users.noreply.github.com>
2021-09-26 15:52:54 -0400

    Update new_trip.py

--- Lab 2/new_trip.py
+++ Lab 2/new_trip.py
@@ -116,7 +116,10 @@ while True:
             
         while end == None:
             if buttonA.value and not buttonB.value:
+                draw.rectangle((0, 0, width, height), outline=0, fill=0)
                 draw.text((0, 0), "you are travelling" + str(dirr), font=font, fill="#F9AD43")
+                disp.image(image, rotation)
+                
                 time.sleep(1)
                 
         disp.image(image, rotation)
