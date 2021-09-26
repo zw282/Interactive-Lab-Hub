0dfc65f97db5155881b5c8598401a18c68763047
zw282 <55099696+zw282@users.noreply.github.com>
2021-09-26 16:35:12 -0400

    Update new_trip.py

--- Lab 2/new_trip.py
+++ Lab 2/new_trip.py
@@ -97,16 +97,14 @@ while True:
     disp.image(image, rotation)
     
     end = None
-    end1 = None
     
     if buttonB.value and not buttonA.value:
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)       
         dirr = dirlist[0]
         draw.text((0,0), str(dirr), font=font, fill ="#F9AD43")
-        START = 'Start traveling'
         disp.image(image, rotation)
         time.sleep(0.5)
+        
         
         if buttonB.value and not buttonA.value:
             draw.rectangle((0, 0, width, height), outline=0, fill=0)
@@ -115,12 +113,12 @@ while True:
             disp.image(image, rotation)
             time.sleep(0.5)
             
-    while end == None:
-        if buttonA.value and not buttonB.value:
-            draw.rectangle((0, 0, width, height), outline=0, fill=0)
-            draw.text((0, 0), "you are travelling " + str(dirr), font=font, fill="#F9AD43")
-            disp.image(image, rotation)            
-            time.sleep(1)   
+        while end == None:
+            if buttonA.value and not buttonB.value:
+                draw.rectangle((0, 0, width, height), outline=0, fill=0)
+                draw.text((0, 0), "you are travelling " + str(dirr), font=font, fill="#F9AD43")
+                disp.image(image, rotation)            
+                time.sleep(1)   
                 
 
 
