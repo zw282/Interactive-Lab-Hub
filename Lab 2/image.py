7c3f527d50b40c78a5ab95201e3c8683dfedb675
rubydongerpan <90153252+rubydongerpan@users.noreply.github.com>
2021-09-27 17:25:37 -0400

    Update image.py

--- Lab 2/image.py
+++ Lab 2/image.py
@@ -74,7 +74,7 @@ draw = ImageDraw.Draw(image)
 draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
 disp.image(image)
 
-image = Image.open("img1.png")
+image = Image.open("red.png")
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
