8ca27029cdd5fcfc3bf6d4b2c75873ac8dc4de39
zw282 <55099696+zw282@users.noreply.github.com>
2021-10-03 17:10:23 -0400

    Update test.sh

--- Lab 3/test.sh
+++ Lab 3/test.sh
@@ -1,6 +1,1 @@
-#https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
-
-#!/bin/bash
-say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
-#say $*
-say " This mission is too important for me to allow you to jeopardize it."
+echo "Just what do you think you're doing, Emma?" | festival --tts
